38da94fd8b0ec2435e6af48b91c0354ad91ebea8
n-aa-g <wlsdnjs0124@gmail.com>
2023-09-01 11:43:37 +0900

    feat: cors 설정 #9

---
+++ be/src/main/java/team05a/secondhand/filter/CorsFilter.java
@@ -0,0 +1,28 @@
+package team05a.secondhand.filter;
+
+import org.springframework.http.HttpHeaders;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:5173, http://13.125.53.49:5173");
+        response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+        response.setHeader(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
+        response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
+                "Authorization, x-requested-with, origin, content-type, accept");
+        if (((HttpServletRequest) servletRequest).getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+        chain.doFilter(servletRequest, servletResponse);
+    }
+}
