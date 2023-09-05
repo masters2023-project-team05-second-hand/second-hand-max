85653d7ee83faa30fa37ff1ee585b907774af9fb
n-aa-g <wlsdnjs0124@gmail.com>
2023-09-05 17:27:46 +0900

    chore: data.sql 수정 #15
    MemberAddress를 테스트하기 위해 data를 추가했습니다.

--- be/src/test/resources/data.sql
+++ be/src/test/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO member (type, email, nickname, profile_img_url)
 VALUE ('KAKAO', 'nag@codesquad.kr', 'nag', 'nagProfile');
+
+INSERT INTO member_address (is_last_visited, address_id, member_id)
+VALUES (1, 1, 1),
+       (0, 10, 1);
