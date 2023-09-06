5ebaf9809fac0ddff6a39833596c1516da06a949
JIHYE <jdh324@naver.com>
2023-09-07 03:12:50 +0900

    feat: 상품 수정 예외 처리 #23

---
+++ be/src/main/java/team05a/secondhand/errors/handler/ProductExceptionHandler.java
@@ -0,0 +1,34 @@
+package team05a.secondhand.errors.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+import team05a.secondhand.errors.ErrorResponse;
+import team05a.secondhand.errors.exception.ProductNotFoundException;
+import team05a.secondhand.errors.exception.UnauthorizedProductModificationException;
+
+@Slf4j
+@RestControllerAdvice
+public class ProductExceptionHandler {
+
+	@ExceptionHandler(ProductNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleProductNotFoundException(ProductNotFoundException e) {
+		log.warn("ProductNotFoundException handling : {}", e.toString());
+
+		ErrorResponse response = ErrorResponse.from(e.getMessage());
+		return ResponseEntity.status(response.getStatus())
+			.body(response);
+	}
+
+	@ExceptionHandler(UnauthorizedProductModificationException.class)
+	public ResponseEntity<ErrorResponse> handleUnauthorizedProductModificationException(
+		UnauthorizedProductModificationException e) {
+		log.warn("UnauthorizedProductModificationException handling : {}", e.toString());
+
+		ErrorResponse response = ErrorResponse.from(e.getMessage());
+		return ResponseEntity.status(response.getStatus())
+			.body(response);
+	}
+}
