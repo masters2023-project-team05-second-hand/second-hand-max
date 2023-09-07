9a6a8536e01752e4b815ab6ea4f6d8d844b7fb39
n-aa-g <wlsdnjs0124@gmail.com>
2023-09-07 16:54:44 +0900

    feat: response에 address와 category 추가 #30
    프론트의 요청에 따라 추가했습니다.

--- be/src/main/java/team05a/secondhand/product/data/dto/ProductDetailResponse.java
+++ be/src/main/java/team05a/secondhand/product/data/dto/ProductDetailResponse.java
@@ -3,19 +3,20 @@ package team05a.secondhand.product.data.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
-import team05a.secondhand.category.data.entity.Category;
+import team05a.secondhand.address.data.dto.ProductAddressResponse;
+import team05a.secondhand.category.data.dto.ProductCategoryResponse;
 import team05a.secondhand.product.data.entity.Product;
-import team05a.secondhand.status.data.entity.Status;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 public class ProductDetailResponse {
 
     private final String seller;
-    private final String category;
+    @JsonProperty("category")
+    private final ProductCategoryResponse productCategoryResponse;
+    @JsonProperty("address")
+    private final ProductAddressResponse productAddressResponse;
     private final String title;
     private final String contents;
     private final String price;
@@ -24,9 +25,10 @@ public class ProductDetailResponse {
     private final Long statusId;
 
     @Builder
-    private ProductDetailResponse(String seller, String category, String title, String contents, String price, Timestamp createdTime, Long statusId) {
+    public ProductDetailResponse(String seller, ProductCategoryResponse productCategoryResponse, ProductAddressResponse productAddressResponse, String title, String contents, String price, Timestamp createdTime, Long statusId) {
         this.seller = seller;
-        this.category = category;
+        this.productCategoryResponse = productCategoryResponse;
+        this.productAddressResponse = productAddressResponse;
         this.title = title;
         this.contents = contents;
         this.price = price;
@@ -37,7 +39,8 @@ public class ProductDetailResponse {
     public static ProductDetailResponse from(Product product) {
         return ProductDetailResponse.builder()
             .seller(product.getMember().getNickname())
-            .category(product.getCategory().getName())
+            .productCategoryResponse(ProductCategoryResponse.from(product.getCategory()))
+            .productAddressResponse(ProductAddressResponse.from(product.getAddress()))
             .title(product.getTitle())
             .contents(product.getContent())
             .price(product.getPrice())
