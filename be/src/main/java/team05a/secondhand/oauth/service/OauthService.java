9e9ffb55b38c1efacc42bd6893524c63b2fdbdc1
n-aa-g <wlsdnjs0124@gmail.com>
2023-09-07 21:52:24 +0900

    fix: jwt를 객체로 매핑

--- be/src/main/java/team05a/secondhand/oauth/service/OauthService.java
+++ be/src/main/java/team05a/secondhand/oauth/service/OauthService.java
@@ -9,6 +9,7 @@ import team05a.secondhand.member.data.entity.Member;
 import team05a.secondhand.member.repository.MemberRepository;
 import team05a.secondhand.member_refreshtoken.data.entity.MemberRefreshToken;
 import team05a.secondhand.member_refreshtoken.repository.MemberRefreshTokenRepository;
+import team05a.secondhand.oauth.data.dto.JwtResponse;
 import team05a.secondhand.oauth.data.dto.MemberOauthRequest;
 
 import java.util.Map;
@@ -22,12 +23,12 @@ public class OauthService {
     private final MemberRefreshTokenRepository memberRefreshTokenRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public Jwt login(MemberOauthRequest memberOauthRequest) {
+    public JwtResponse login(MemberOauthRequest memberOauthRequest) {
         Member member = save(memberOauthRequest);
         Jwt jwt = jwtTokenProvider.createJwt(Map.of("memberId", member.getId()));
         memberRefreshTokenRepository.save(new MemberRefreshToken(member, jwt.getRefreshToken()));
 
-        return jwt;
+        return JwtResponse.builder().tokens(jwt).build();
     }
 
     private Member save(MemberOauthRequest memberOauthRequest) {
