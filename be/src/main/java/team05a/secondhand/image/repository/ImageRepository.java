25d6c5fc8242fb5ed3488738b2cc09e919f89370
n-aa-g <wlsdnjs0124@gmail.com>
2023-09-07 18:03:14 +0900

    chore: import 추가

--- be/src/main/java/team05a/secondhand/image/repository/ImageRepository.java
+++ be/src/main/java/team05a/secondhand/image/repository/ImageRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import team05a.secondhand.image.data.entity.ProductImage;
+import team05a.secondhand.product.data.entity.Product;
 
 import java.util.List;
 
