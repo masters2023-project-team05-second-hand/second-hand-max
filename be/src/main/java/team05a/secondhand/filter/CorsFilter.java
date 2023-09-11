7a92aa4e4ab519960e770e4bb094789a7631c002
n-aa-g <wlsdnjs0124@gmail.com>
2023-09-11 22:11:02 +0900

    build: ci/cd 파일 추가

--- be/src/main/java/team05a/secondhand/filter/CorsFilter.java
+++ be/src/main/java/team05a/secondhand/filter/CorsFilter.java
@@ -12,14 +12,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpHeaders;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class CorsFilter implements Filter {
+
+	private final String allowedOrigin;
 
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
 		throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse)servletResponse;
 
-		response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:5173");
+		response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, allowedOrigin);
 		response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, PATCH, DELETE, OPTIONS");
 		response.setHeader(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
 		response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
