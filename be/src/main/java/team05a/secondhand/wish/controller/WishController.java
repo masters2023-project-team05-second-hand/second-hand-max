b04992e90f17c00783a85a0308e5f0e806311dcc
nag0124 <wlsdnjs0124@gmail.com>
2023-09-19 15:16:46 +0900

    [BE]: 관심상품 조회 기능 구현 (#78)
    * feat: 관심 목록 조회 기능 구현 #77
    
    * test: 관심 목록 조회 기능 통합 테스트 작성 #77

--- be/src/main/java/team05a/secondhand/wish/controller/WishController.java
+++ be/src/main/java/team05a/secondhand/wish/controller/WishController.java
@@ -2,16 +2,19 @@ package team05a.secondhand.wish.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import team05a.secondhand.member.resolver.MemberId;
+import team05a.secondhand.product.data.dto.ProductListResponse;
 import team05a.secondhand.wish.data.dto.WishCategoryResponse;
 import team05a.secondhand.wish.data.dto.WishRequest;
 import team05a.secondhand.wish.data.dto.WishResponse;
@@ -23,6 +26,15 @@ import team05a.secondhand.wish.service.WishService;
 public class WishController {
 
 	private final WishService wishService;
+
+	@GetMapping
+	public ResponseEntity<ProductListResponse> readWishProducts(@MemberId Long memberId,
+		@RequestParam(required = false) Long categoryId, Pageable pageable) {
+		ProductListResponse productListResponse = wishService.getWishProducts(memberId, categoryId, pageable);
+
+		return ResponseEntity.ok()
+			.body(productListResponse);
+	}
 
 	@PostMapping()
 	public void createWish(@MemberId Long memberId, @RequestBody WishRequest wishRequest) {
--- be/src/main/java/team05a/secondhand/wish/repository/WishRepositoryCustom.java
+++ be/src/main/java/team05a/secondhand/wish/repository/WishRepositoryCustom.java
@@ -2,9 +2,16 @@ package team05a.secondhand.wish.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+
 import team05a.secondhand.category.data.entity.Category;
+import team05a.secondhand.product.data.dto.ProductListResponse;
 
 public interface WishRepositoryCustom {
 
 	List<Category> findWishCategoriesByMemberId(Long memberId);
+
+	ProductListResponse findWishProductsByMemberId(Long memberId, Pageable pageable);
+
+	ProductListResponse findWishProductsByMemberIdAndCategoryId(Long memberId, Long categoryId, Pageable pageable);
 }
