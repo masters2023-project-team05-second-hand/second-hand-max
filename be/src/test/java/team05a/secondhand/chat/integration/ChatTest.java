da74c7e20bbc0cb5c38ae113fbf037221f303e37
n-aa-g <wlsdnjs0124@gmail.com>
2023-10-03 15:23:30 +0900

    test: 구매자와 메세지 송신자 일치 여부 검증 기능 테스트 작성 #89

--- be/src/test/java/team05a/secondhand/chat/integration/ChatTest.java
+++ be/src/test/java/team05a/secondhand/chat/integration/ChatTest.java
@@ -52,8 +52,11 @@ public class ChatTest {
 		memberRepository.save(member);
 		Product product = FixtureFactory.createProductRequest(member);
 		productRepository.save(product);
-		String accessToken = jwtTokenProvider.createAccessToken(Map.of("memberId", member.getId()));
-		String requestBody = "{\"productId\": 1,\"message\": {\"senderId\": 1,\"content\": \"얼마야?\"}}";
+		Member sender = FixtureFactory.createAnotherMember();
+		memberRepository.save(sender);
+		String accessToken = jwtTokenProvider.createAccessToken(Map.of("memberId", sender.getId()));
+		String requestBody = "{\"productId\": 1,\"message\": {\"senderId\": " + sender.getId()
+			+ ",\"content\": \"얼마야?\"}}";
 
 		//when
 		ResultActions resultActions = mockMvc.perform(
@@ -69,5 +72,34 @@ public class ChatTest {
 		resultActions
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.roomId").isNotEmpty());
+	}
+
+	@Test
+	@DisplayName("채팅방을 만들려할 때 구매자 아이디와 메세지 전송자 아이디가 같지 않으면 예외를 던진다.")
+	void createChatRoomWithNotSameBuyerIdAndSenderId() throws Exception {
+		//given
+		Member member = FixtureFactory.createMember();
+		memberRepository.save(member);
+		Product product = FixtureFactory.createProductRequest(member);
+		productRepository.save(product);
+		Member sender = FixtureFactory.createAnotherMember();
+		memberRepository.save(sender);
+		String accessToken = jwtTokenProvider.createAccessToken(Map.of("memberId", sender.getId()));
+		String requestBody = "{\"productId\": 1,\"message\": {\"senderId\": " + (sender.getId() + 1)
+			+ ",\"content\": \"얼마야?\"}}";
+
+		//when
+		ResultActions resultActions = mockMvc.perform(
+				MockMvcRequestBuilders
+					.post("/api/chat/room")
+					.header("Authorization", "Bearer " + accessToken)
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(requestBody)
+			)
+			.andDo(print());
+
+		//given
+		resultActions
+			.andExpect(status().isBadRequest());
 	}
 }
