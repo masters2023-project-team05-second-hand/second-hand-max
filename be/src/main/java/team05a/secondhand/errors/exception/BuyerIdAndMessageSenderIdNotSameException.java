9b2ea91eae3646617dd2e2ea813f6f0b08ff689f
n-aa-g <wlsdnjs0124@gmail.com>
2023-10-03 18:10:52 +0900

    sytle: 메세지 변경 #89
    - 에러 메세지에 잘못된 메세지가 들어가 있어 변경했습니다.

--- be/src/main/java/team05a/secondhand/errors/exception/BuyerIdAndMessageSenderIdNotSameException.java
+++ be/src/main/java/team05a/secondhand/errors/exception/BuyerIdAndMessageSenderIdNotSameException.java
@@ -2,7 +2,7 @@ package team05a.secondhand.errors.exception;
 
 public class BuyerIdAndMessageSenderIdNotSameException extends RuntimeException {
 
-	private static final String MESSAGE = "구매자 아이디와 첫 메세지 발송자 아이디가 일치합니다.";
+	private static final String MESSAGE = "구매자 아이디와 첫 메세지 발송자 아이디가 일치하지 않습니다.";
 
 	public BuyerIdAndMessageSenderIdNotSameException() {
 		super(MESSAGE);
