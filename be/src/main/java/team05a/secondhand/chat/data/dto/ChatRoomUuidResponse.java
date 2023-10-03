1d00225c6b10847aa4751324e38872906175fb02
n-aa-g <wlsdnjs0124@gmail.com>
2023-10-03 22:35:05 +0900

    feat: 채팅방 생성여부 확인 기능 구현 #91

--- be/src/main/java/team05a/secondhand/chat/data/dto/ChatRoomUuidResponse.java
+++ be/src/main/java/team05a/secondhand/chat/data/dto/ChatRoomUuidResponse.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Builder;
 import lombok.Getter;
+import team05a.secondhand.chat.data.entity.ChatRoom;
 
 @Getter
 public class ChatRoomUuidResponse {
@@ -14,5 +15,16 @@ public class ChatRoomUuidResponse {
 	@Builder
 	private ChatRoomUuidResponse(String roomUuid) {
 		this.roomUuid = roomUuid;
+	}
+
+	public static ChatRoomUuidResponse from(ChatRoom chatRoom) {
+		if (chatRoom == null) {
+			return ChatRoomUuidResponse.builder()
+				.roomUuid(null)
+				.build();
+		}
+		return ChatRoomUuidResponse.builder()
+			.roomUuid(chatRoom.getUuid())
+			.build();
 	}
 }
