c0286f6a291b6170100b119e87c2510ab5ced0fd
n-aa-g <wlsdnjs0124@gmail.com>
2023-10-03 15:54:37 +0900

    test: 구매자와 판매자 일치 여부 검증 기능 테스트 #89

--- be/src/test/java/team05a/secondhand/chat/integration/ChatTest.java
+++ be/src/test/java/team05a/secondhand/chat/integration/ChatTest.java
@@ -17,6 +17,8 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import team05a.secondhand.DatabaseCleanup;
+import team05a.secondhand.errors.exception.BuyerIdAndMessageSenderIdNotSameException;
+import team05a.secondhand.errors.exception.SellerIdAndBuyerIdSameException;
 import team05a.secondhand.fixture.FixtureFactory;
 import team05a.secondhand.jwt.JwtTokenProvider;
 import team05a.secondhand.member.data.entity.Member;
@@ -87,6 +89,7 @@ public class ChatTest {
 		String accessToken = jwtTokenProvider.createAccessToken(Map.of("memberId", sender.getId()));
 		String requestBody = "{\"productId\": 1,\"message\": {\"senderId\": " + (sender.getId() + 1)
 			+ ",\"content\": \"얼마야?\"}}";
+		BuyerIdAndMessageSenderIdNotSameException exception = new BuyerIdAndMessageSenderIdNotSameException();
 
 		//when
 		ResultActions resultActions = mockMvc.perform(
@@ -100,6 +103,38 @@ public class ChatTest {
 
 		//given
 		resultActions
-			.andExpect(status().isBadRequest());
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.message")
+				.value(exception.getMessage()));
+	}
+
+	@Test
+	@DisplayName("채팅방을 만들려할 때 구매자 아이디와 판매자 아이디가 같으면 예외를 던진다.")
+	void createChatRoomWithNotSameBuyerIdAndSellerId() throws Exception {
+		//given
+		Member member = FixtureFactory.createMember();
+		memberRepository.save(member);
+		Product product = FixtureFactory.createProductRequest(member);
+		productRepository.save(product);
+		String accessToken = jwtTokenProvider.createAccessToken(Map.of("memberId", member.getId()));
+		String requestBody = "{\"productId\": 1,\"message\": {\"senderId\": " + member.getId()
+			+ ",\"content\": \"얼마야?\"}}";
+		SellerIdAndBuyerIdSameException exception = new SellerIdAndBuyerIdSameException();
+
+		//when
+		ResultActions resultActions = mockMvc.perform(
+				MockMvcRequestBuilders
+					.post("/api/chat/room")
+					.header("Authorization", "Bearer " + accessToken)
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(requestBody)
+			)
+			.andDo(print());
+
+		//given
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.message")
+				.value(exception.getMessage()));
 	}
 }
