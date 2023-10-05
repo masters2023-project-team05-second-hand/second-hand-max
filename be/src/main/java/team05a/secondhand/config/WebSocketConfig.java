ee2c853c64aef85216f2cc3a11ee23d4f1485aaa
n-aa-g <wlsdnjs0124@gmail.com>
2023-10-05 15:41:02 +0900

    feat: Jwt 유효성 검증, memberId 추출 기능 구현 #96

--- be/src/main/java/team05a/secondhand/config/WebSocketConfig.java
+++ be/src/main/java/team05a/secondhand/config/WebSocketConfig.java
@@ -1,14 +1,21 @@
 package team05a.secondhand.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import lombok.RequiredArgsConstructor;
+import team05a.secondhand.interceptor.ChatPreHandler;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	private final ChatPreHandler chatPreHandler;
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -19,5 +26,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.enableSimpleBroker("/sub");
 		registry.setApplicationDestinationPrefixes("/pub");
+	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(chatPreHandler);
 	}
 }
